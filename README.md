574ee5aeea6fa98f4f27ac478c8a6365bf4c7090
barbarafarias <barbara.fs@gmail.com>
2020-03-11 16:58:17 +0100

    fix(calculus): fix validation and add documentation

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# calculus
+# Calculus API
+
+## How to run
+
+### With Docker
+```sh
+$ git clone git@github.com:barbarafarias/calculus.git
+$ cd calculus
+$ docker-compose up
+$ Go to http://localhost:3000/calculus?query=Mis4KjktKDEwLzQp
+```
+
+### Without Docker
+```sh
+$ git clone git@github.com:barbarafarias/calculus.git
+$ cd calculus
+$ npm install
+$ npm start
+$ Go to http://localhost:3000/calculus?query=Mis4KjktKDEwLzQp
+```
--- test/unit/validate.spec.js
+++ test/unit/validate.spec.js
@@ -3,12 +3,12 @@ const validate = require('../../lib/helpers/validate')
 
 
 test('validate the query', (t) => {
-    const validQuery = '2*99+5*(8-4)'
-    const invalidQuery = '5<89'
 
-    t.true(validate(validQuery), 'should return true when valid')
-    t.false(validate(invalidQuery), 'should return true when valid')
-    
+    t.true(validate('2*99+5*(8-4)'), 'should return true when valid')
+    t.true(validate('2-(8+5+9+0)*2'), 'should return true when valid')
+    t.true(validate('(50/4)+5'), 'should return true when valid')
+    t.true(validate('80'), 'should return true when valid')
+    t.false(validate('5<pb10'), 'should return false when invalid')
 
     t.end()
 })
